a3d98acf85def3b9938bb93fed443fbade3bbd0a
WhiteShadow <aayushkuinkel41n1@gmail.com>
2025-06-22 19:42:28 +0545

    folder adjustment

--- Frontend/src/component/Admin/WeblayoutComponent.jsx
+++ Frontend/src/component/Admin/WeblayoutComponent.jsx
@@ -1,18 +1,19 @@
 import React from "react";
-import HeaderComponent from "../Common/HeaderComponent";
-import SidebarComponent from "../Common/SidebarComponent";
-import FooterComponent from "../Common/FooterComponent";
+import HeaderComponent from "../Common/Dashboard/HeaderComponent";
+import SidebarComponent from "../Common/Dashboard/SidebarComponent";
+import FooterComponent from "../Common/Dashboard/FooterComponent";
+
 
 const WeblayoutComponent = ({ children }) => {
   return (
     <>
       <div className="d-flex flex-column" style={{ minHeight: "100vh" }}>
-        <HeaderComponent />
+        <HeaderComponent/>
         <div className="d-flex flex-grow-1">
-          <SidebarComponent />
+          <SidebarComponent/>
           <main className="flex-grow-1 p-4 bg-light">{children}</main>
         </div>
-        <FooterComponent />
+        <FooterComponent/>
       </div>
     </>
   );
