43c9cc3b02e047b474cdbd815014e3f43cdabcfa
WhiteShadow <aayushkuinkel41n1@gmail.com>
2025-06-22 12:10:30 +0545

    seller folders add

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,17 +5,23 @@ import ProductsPage from "./pages/Admin/ProductsPage";
 import CreateProduct from "./pages/Admin/CreateProduct";
 import Signup from "./pages/Common/Signup";
 import Login from "./pages/Common/Login";
+import Homepage from "./pages/Common/Homepage";
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Signup/>} />
+          {/* Public Route */}
+          <Route path="/" element={<Homepage/>} />
           <Route path="/login" element={<Login/>} />
+          <Route path="/signup" element={<Signup/>} />
+          <Route path="/login" element={<Login/>} />
+          {/* Should be in private route */}
           <Route path="/dashboard" element={<DashboardPage/>} />
           <Route path="/product-list" element={<ProductsPage />} />
           <Route path="/create-product" element={<CreateProduct />} />
+
         </Routes>
       </BrowserRouter>
     </>
