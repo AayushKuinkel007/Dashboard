db0a152dd614dffff8cc53f1b4974026908202f5
WhiteShadow <aayushkuinkel41n1@gmail.com>
2025-06-23 21:54:39 +0545

    login route and controller add

--- Backend/controllers/usercontroller.js
+++ Backend/controllers/usercontroller.js
@@ -43,3 +43,21 @@ exports.sendUserData = async (req, res) => {
     });
   }
 };
+
+exports.loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+      token: generateToken(user._id, user.role)
+    });
+  } else {
+    res.status(401).json({ message: 'Invalid email or password' });
+  }
+};
--- Backend/routes/userroute.js
+++ Backend/routes/userroute.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const userRouter = express.Router();
 
-const { sendUserData } = require("../controllers/usercontroller");
+const { sendUserData, loginUser } = require("../controllers/usercontroller");
 
 userRouter.post("/senduserdata", sendUserData);
-
+userRouter.post('/login',loginUser)
 module.exports = userRouter;
