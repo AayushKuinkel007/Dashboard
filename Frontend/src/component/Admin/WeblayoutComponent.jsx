643d469085dc62a7039a5b2bf062d04ce7119015
WhiteShadow <aayushkuinkel41n1@gmail.com>
2025-07-02 08:29:43 +0545

    minor commit

--- Frontend/src/component/Admin/WeblayoutComponent.jsx
+++ Frontend/src/component/Admin/WeblayoutComponent.jsx
@@ -3,19 +3,16 @@ import SidebarComponent from "./AdminDashboard/SidebarComponent";
 import FooterComponent from "./AdminDashboard/FooterComponent";
 import HeaderComponent from "./AdminDashboard/HeaderComponent";
 
-
-
-
 const WeblayoutComponent = ({ children }) => {
   return (
     <>
       <div className="d-flex flex-column" style={{ minHeight: "100vh" }}>
-        <HeaderComponent/>
+        <HeaderComponent />
         <div className="d-flex flex-grow-1">
-          <SidebarComponent/>
+          <SidebarComponent />
           <main className="flex-grow-1 p-4 bg-light">{children}</main>
         </div>
-        <FooterComponent/>
+        <FooterComponent />
       </div>
     </>
   );
